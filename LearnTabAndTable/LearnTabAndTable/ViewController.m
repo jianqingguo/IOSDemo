d11458249fbb8079c367b3c42cc43de9b3ca91f5
jianqingguo <guojianqing123@gmail.com>
2018-03-17 12:28:09 +0800

    初始化练习uitabbarcontroller和uitableviewcontroller

---
+++ LearnTabAndTable/LearnTabAndTable/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnTabAndTable
+//
+//  Created by laura on 2018/3/17.
+//  Copyright © 2018年 zm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
